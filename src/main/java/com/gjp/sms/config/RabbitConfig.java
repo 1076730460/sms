4b8c31ee8b9c6228f71d826d4b13fcdd3e2eb3ad
gengjiaping <1076730460@qq.com>
2019-04-12 18:01:46 +0800

    加日志和rabitmq

---
+++ src/main/java/com/gjp/sms/config/RabbitConfig.java
@@ -0,0 +1,21 @@
+package com.gjp.sms.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *<p>RabbitMQ配置</p>
+ * @author 耿加平
+ * @date  2019-04-12
+ */
+
+
+public class RabbitConfig {
+    private Logger log = LoggerFactory.getLogger(RabbitConfig.class);
+
+    public void rabbitTemplate(){
+
+    }
+}
