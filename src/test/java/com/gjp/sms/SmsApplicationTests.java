7b2fabe6229c8d5c72a78c1d74e62e0c11a891ce
gengjiaping <1076730460@qq.com>
2019-05-05 17:47:15 +0800

    sms短信发送接口测试成功

--- src/test/java/com/gjp/sms/SmsApplicationTests.java
+++ src/test/java/com/gjp/sms/SmsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.gjp.sms;
 
 import com.gjp.sms.common.util.HttpClientUtil;
+import com.gjp.sms.common.util.SendMessageUtil;
 import com.gjp.sms.message.SendMessage;
 import com.gjp.sms.service.MailService;
 import org.junit.Test;
@@ -18,6 +19,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+
+import static com.gjp.sms.common.util.SendMessageUtil.getRandomCode;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -78,6 +81,16 @@ public class SmsApplicationTests {
         param.put("sendtime", sendtime);
        param.put("server", "http://sms.webchinese.cn/web_api/");
         String smsRet = HttpClientUtil.post("http://sms.webchinese.cn/web_api/", param);
+    }
+
+    /**
+     * sms短信验证成功
+     */
+    @Test
+    public void testSendMessage(){
+//        SendMessageUtil.send("SMS账户","接口秘钥","目标号码","发送内容");
+       // SendMessageUtil.send("gengjiaping","d41d8cd98f00b204e980","18108753351","验证码:"+getRandomCode(6));
+        SendMessageUtil.send("gengjiaping","d41d8cd98f00b204e980","17787109883","存宝宝");
     }
 
 }
